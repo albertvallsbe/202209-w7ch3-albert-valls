39ee9106ee9692c206d95753d9b67dcce2721832
Albert Valls <avalls89@gmail.com>
2022-11-09 18:58:54 +0100

    Add connect to ddbb

---
+++ src/database/index.ts
@@ -0,0 +1,18 @@
+import "../loadEnviorments.js";
+import mongoose from "mongoose";
+import chalk from "chalk";
+import debugCreator from "debug";
+
+const debug = debugCreator(`${process.env.DEBUG}`);
+
+export const connectToDb = async (url: string) => {
+  try {
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+    await mongoose.connect(url, { dbName: "w7ch3" });
+    debug(chalk.yellow("Connect to database"));
+  } catch {
+    debug(chalk.red("Error to connect to DDBB"));
+  }
+};
+
+export default connectToDb;
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,6 @@
+import connectToDb from "./database/index.js";
 import "./loadEnviorments.js";
+
+const { MONGO_URL: url } = process.env;
+
+await connectToDb(url);
